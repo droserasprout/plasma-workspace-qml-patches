0c67f7a1f162cc0300c48e23b9ef5dfd88ae557a
droserasprout <droserasprout@tuta.io>
2017-05-31 22:21:55 +0000

    updated to 5.10.0

--- apply-patches.sh
+++
@@ -1,7 +0,0 @@
-echo "###### Patching plasmashell"
-(cd / && sudo patch -p0 -b) < removeTrayHeadings.patch
-echo "###### Patching kscreenlocker"
-(cd / && sudo patch -p0 -b) < addLockscreenBackground.patch
-echo "###### Patching sddm-greeter"
-(cd / && sudo patch -p0 -b) < addGreeterBackground.patch
-(cd / && sudo patch -p0 -b) < removeGreeterLabels.patch
--- reverse-patches.sh
+++
@@ -1,7 +0,0 @@
-echo "###### Patching plasmashell"
-(cd / && sudo patch -p0 -R) < removeTrayHeadings.patch
-echo "###### Patching kscreenlocker"
-(cd / && sudo patch -p0 -R) < addLockscreenBackground.patch
-echo "###### Patching sddm-greeter"
-(cd / && sudo patch -p0 -R) < addGreeterBackground.patch
-(cd / && sudo patch -p0 -R) < removeGreeterLabels.patch
